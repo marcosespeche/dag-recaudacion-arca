c8ea31eb2dc1e693ee16b741ec6993fc15fd5df9
marcosespeche <marcos.espeche.28@gmail.com>
2025-08-31 19:08:17 -0300

    Añadiendo tasks para descargar recaudacion de ARCA y EMAE

--- dags/dag_montos.py
+++ dags/dag_montos.py
@@ -20,7 +20,7 @@ INITIAL_YEAR = 2008
 LAST_YEAR = 2025
 URL_ARCHIVO_IPC_ARGENTINA = "/ftp/cuadros/economia/sh_ipc_08_25.xls"
 URL_ARCHIVO_IPC_CORDOBA = "/dataset/fedc5285-5517-41aa-9095-bb62c6dbc485/resource/2b4a7c60-1c8a-45b1-be8f-2bd59bfe2364/download/ipc-cba-julio.xlsx"
-BASE_URL_ARCHIVO_RECAUDACION = "https://contenidos.afip.gob.ar/institucional/estudios/archivos/serie"
+URL_ARCHIVO_RECAUDACION = "/institucional/estudios/archivos/serie"
 PATH_ARCHIVOS_RECAUDACION = "/tmp/recaudacion/"
 PATH_ARCHIVOS_IPC = "/tmp/ipc/"
 PATH_ARCHIVOS_EMAE = "/tmp/emae/"
@@ -196,7 +196,96 @@ def descargar_ipc_cordoba(**kwards):
         raise e
 
 def descargar_archivos_recaudacion(**kwards):
-    return
+    hook = HttpHook(http_conn_id='recaudacion-arca', method='GET')
+    csv_exporter = CSVExporter()
+    try:
+        for year in range(INITIAL_YEAR, LAST_YEAR + 1):
+            data = obtener_hoja_xls(hook, f"{URL_ARCHIVO_RECAUDACION}{year}.xls", f"{year}")
+
+            nombre_fila_recaudacion = '  TOTAL GENERAL'
+            idx_fila_recaudacion = None
+
+            for index, row in enumerate(data):
+                if (row[1] != nombre_fila_recaudacion):
+                    continue
+                idx_fila_recaudacion = index
+
+                for month in range(1, 13):
+                    column_index = month + 1
+                    recaudacion = row[column_index]
+                    if year < 2021:
+                        # Antes de 2021: convertir de miles a millones
+                        recaudacion = recaudacion / 1000
+                    if recaudacion is not None and recaudacion != '':
+                        # No agarra el valor total (la sumatoria de todos los meses) porque está definido como una función de Excel, no es un valor numerico en sí
+                        csv_exporter.add(year, month, recaudacion)
+                break
+
+            if idx_fila_recaudacion is None:
+                logging.warning(f"No se encontró la fila con los totales de recaudación para el año {year}")
+
+        return csv_exporter.export(PATH_ARCHIVOS_RECAUDACION, 'recaudacion-arca')
+    
+    except Exception as e:
+        logging.error(f"[ERROR] Problema al descargar la recaudación de ARCA: {e}")
+        raise e
+
+def descargar_emae(**kwards):
+    hook = HttpHook(http_conn_id='emae', method='GET')
+    csv_exporter = CSVExporter()
+    
+    MESES_MAP = { 'Enero': 1, 'Febrero': 2, 'Marzo': 3, 'Abril': 4, 'Mayo': 5, 'Junio': 6, 'Julio': 7, 'Agosto': 8, 'Septiembre': 9, 'Octubre': 10, 'Noviembre': 11, 'Diciembre': 12 }
+
+    current_year = None
+    meses_por_anio = {}
+
+    try:
+        data = obtener_hoja_xls(hook, 'sh_emae_mensual_base2004.xls', 'EMAE')
+
+        """
+            Formato del Excel:
+
+            2024 Enero valor1
+                 Febrero valor 2
+                 marzo valor3
+            ...
+            2025 Enero valor4
+
+            Es decir, hay filas que contienen el año y filas que no 
+            """
+
+        for index, row in enumerate(data):
+            anio = row[0]
+            mes = row[1] 
+            valor = row[2]
+
+            # Si se detecta un nuevo año, se pisa el valor de current_year anterior y añaden los datos registrados hasta el momento en el CSV
+            if isinstance(anio, (int, float)) and INITIAL_YEAR <= int(anio) <= LAST_YEAR:
+                if current_year and meses_por_anio:
+                    for m, v in meses_por_anio.items():
+                        csv_exporter.add(current_year, m, v)
+                    meses_por_anio.clear()
+
+                current_year = int(anio)
+
+            # Aun no se llega a los datos del Excel, se saltea la fila
+            elif current_year is None:
+                continue  
+
+            # Registrar valor del mes si es válido
+            if mes and mes in MESES_MAP and valor is not None:
+                meses_por_anio[MESES_MAP[mes]] = valor
+
+        # Volcar lo último acumulado
+        if current_year and meses_por_anio:
+            for m, v in meses_por_anio.items():
+                csv_exporter.add(current_year, m, v)
+
+    except Exception as e:
+        logging.error(f"Error al descargar el EMAE: {e}")
+        raise e
+
+    return csv_exporter.export(PATH_ARCHIVOS_EMAE, 'emae')
 
 with DAG(
     dag_id='montos',
@@ -222,7 +311,12 @@ with DAG(
         python_callable = descargar_archivos_recaudacion
     )
 
-    [task_descargar_ipc_argentina, task_descargar_ipc_cordoba, task_descargar_archivos_recaudacion]
+    task_descargar_emae = PythonOperator(
+        task_id = 'task_descargar_emae',
+        python_callable = descargar_emae
+    )
+
+    [task_descargar_ipc_argentina, task_descargar_ipc_cordoba, task_descargar_archivos_recaudacion, task_descargar_emae]
     # [task_descargar_ipc_argentina, task_descargar_ipc_gba, task_descargar_ipc_cordoba, task_descargar_ipc_santafe, task_descargar_ipc_mendoza, task_descargar_ipc_tucuman, task_descargar_archivos_recaudacion, task_descargar_emae] >> task_merge
 
 # El csv para la poblacion zona (String), poblacion, solo para las provincias que tengamos IPC
--- extraer_resultados.bat
+++ extraer_resultados.bat
@@ -1,1 +1,2 @@
 docker cp dag-recaudacion-arca_b5ea64-scheduler-1:/tmp/ipc/ ./resultados
+docker cp dag-recaudacion-arca_538fda-scheduler-1:/tmp/recaudacion/ ./resultados
